92f6534b2728541906c7d4705236348df127a361
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-07-18 10:46:36 -0700

    Use dnspython for reverse lookups

--- network.py
+++ network.py
@@ -1,19 +1,29 @@
+import dns.exception
 import dns.resolver
+import dns.reversename
 
+def get_record(hostname, record_type="A"):
+    """Returns the first record, or None, of the requested type."""
+    try:
+        resp = dns.resolver.query(hostname, record_type)
+        return resp[0].to_text()
+    except (dns.resolver.NXDOMAIN, dns.exception.SyntaxError, IndexError):
+        return None
 
 def get_ip(hostname):
     """Returns IP address for hostname. IPv4 only (for now!)."""
-    try:
-        resp = dns.resolver.query(hostname, "A")
-        return resp[0].address
-    except (dns.resolver.NXDOMAIN, IndexError):
-        return None
+    return get_record(hostname, record_type="A")
 
 
-def get_reverse_dns(ip, timeout=2):
+def get_reverse_dns(ip):
     """Returns reverse DNS record for IP, if it exists."""
     try:
-        socket.setdefaulttimeout(timeout)
-        result = socket.gethostbyaddr(ip)
-    except socket.herror:
-        raise NameError("Unable to resolve IP `{}` to PTR".format(ip))
+        arpa = dns.reversename.from_address(ip)
+    except dns.exception.SyntaxError:
+        return None
+
+    record = get_record(arpa, "PTR")
+
+    # remove trailing dot
+    if record:
+        return record[:-1]
