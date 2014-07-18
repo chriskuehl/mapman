761c6f14d54f4feeb01330aea613a5cddda80b6f
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-07-18 10:33:21 -0700

    Add port scanning

--- scan.py
+++ scan.py
@@ -29,14 +29,27 @@ def get_hosts(network):
             "vendor": vendor_name
         }
 
-def get_services(hosts):
-    """Returns a list of services for the specified list of IP hosts (given as
+
+def get_open_ports(hosts):
+    """Returns a list of open ports for the specified list of IP hosts (given as
     IP addresses)."""
-    pass
+
+    nm = nmap.PortScanner()
+    nm.scan(", ".join(hosts), arguments="--top-ports 25 -sT")
+
+    def open_ports(host):
+        """Returns list of open ports for given host."""
+        tcp = nm[host]["tcp"]
+        return filter(lambda port: tcp[port]["state"] == "open", tcp)
+
+    return {host: open_ports(host) for host in nm.all_hosts()}
+
 
 def main():
-    for host in get_hosts("169.229.10.0/24"):
-        print(host)
+    test_hosts = ["169.229.10.200", "169.229.10.25", "169.229.10.2"]
+
+    for host, ports in get_open_ports(test_hosts).items():
+        print("{}: {}".format(host, list(ports)))
 
 if __name__ == "__main__":
     main()
